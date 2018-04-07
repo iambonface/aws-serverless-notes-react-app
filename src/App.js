fba549804904f6bff66d7b99aa94f3bfcfc459e9
iambonface <mudirib@gmail.com>
2018-04-07 23:28:31 +0300

    Modify App.js to clear session on logout

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ class App extends Component {
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   }
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
       this.userHasAuthenticated(false);
   }
   render() {
