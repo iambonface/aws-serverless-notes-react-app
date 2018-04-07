1ea75de927676e68b7e764091ddd5f77767098c4
iambonface <mudirib@gmail.com>
2018-04-07 19:57:21 +0300

    Created Home container

---
+++ src/containers/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+	render() {
+		return (
+			<div className="Home">
+				<div className="lander">
+					<h1>Scratch</h1>
+					<p>Note taking app </p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Home;
