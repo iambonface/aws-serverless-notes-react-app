43c30bc012c30dcc2c3437b04c741c6e2ff1d18b
iambonface <mudirib@gmail.com>
2018-04-07 23:34:29 +0300

    Update App.js to redirect to Login after Logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import Routes from "./Routes";
@@ -37,6 +37,7 @@ class App extends Component {
   handleLogout = async event => {
     await Auth.signOut();
       this.userHasAuthenticated(false);
+      this.props.history.push("/login");
   }
   render() {
     const childProps = {
@@ -75,4 +76,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
